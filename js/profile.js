29dc69c488dde0dda6db174957def0e7f133df8f
freeleearte <chungho070@gmail.com>
2025-04-23 13:15:18 +0900

    profile update

--- js/profile.js
+++ js/profile.js
@@ -48,47 +48,47 @@ var nodes = new vis.DataSet([
     {
         id: 1,
         shape: 'image',
-        image: '../asset/kr.png',
+        image: './asset/kr.png',
     },
     {
         id: 2,
         shape: 'image',
-        image: '../asset/us.png',
+        image: './asset/us.png',
     },
     {
         id: 3,
         shape: 'image',
-        image: '../asset/css.png',
+        image: './asset/css.png',
     },
     {
         id: 4,
         shape: 'image',
-        image: '../asset/figma.png',
+        image: './asset/figma.png',
     },
     {
         id: 5,
         shape: 'image',
-        image: '../asset/gpt.png',
+        image: './asset/gpt.png',
     },
     {
         id: 6,
         shape: 'image',
-        image: '../asset/html.png',
+        image: './asset/html.png',
     },
     {
         id: 7,
         shape: 'image',
-        image: '../asset/illust.png',
+        image: './asset/illust.png',
     },
     {
         id: 8,
         shape: 'image',
-        image: '../asset/js.png',
+        image: './asset/js.png',
     },
     {
         id: 9,
         shape: 'image',
-        image: '../asset/ps.png',
+        image: './asset/ps.png',
     },
 ]);
 
@@ -110,15 +110,15 @@ nodes: {
       vadjust: 10
     }
   } */
-var edges = new vis.DataSet();
+let edges = new vis.DataSet();
 
-var container = document.getElementById('bubbles_i');
-var data = {
+let container = document.getElementById('bubbles_i');
+let data = {
     nodes: nodes,
     edges: edges
 };
 
-var options = {
+let options = {
     nodes: { borderWidth: 0, shape: "circle", color: { background: '#F5A623', highlight: { background: '#F5A623', border: '#F5A623' } }, font: { color: '#fff' } },
     physics: {
         stabilization: false,
@@ -132,7 +132,7 @@ var options = {
         zoomView: false
     }
 };
-var network = new vis.Network(container, data, options);
+let network = new vis.Network(container, data, options);
 
 
 // Events
@@ -143,6 +143,23 @@ network.on("click", function (e) {
         nodes.update(node);
     }
 });
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second_p",
+        start: "center center",
+        end: "bottom center",
+        scroller: scrollContainer,
+        scrub: true,
+    }
+})
+    .to("#bubbles_i", {
+        top: 100 + '%',
+        opacity: 0,
+        scale: 2,
+        duration: 1,
+        ease: "power1.out",
+    });
 
 // hole 애니메이션 등장 트리거
 gsap.timeline({
@@ -213,3 +230,110 @@ tl.to('.last_f strong.l_txt', {
     duration: 1,
     ease: 'none'
 }, '+=0.6');
+
+const cubeWrap = document.querySelector('.p_cube_wrap');
+const cube_width = cubeWrap.offsetWidth;
+const cube_height = cubeWrap.offsetHeight;
+
+// 장면(Scene) 생성
+let scene = new THREE.Scene();
+// scene.background = new THREE.Color(0x4BCED2);
+
+let camera = new THREE.PerspectiveCamera(
+    75,
+    cube_width / cube_height,
+    0.1,
+    1000
+);
+
+camera.position.set(5, 5, 15);
+
+// 렌더러 생성 및 설정
+let renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+renderer.setSize(cube_width, cube_height);
+document.getElementById("myCanvasContainer").appendChild(renderer.domElement);
+
+// 컨트롤 추가
+let controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+// 빛 추가
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
+scene.add(ambientLight);
+
+let loader = new THREE.TextureLoader();
+let material = [
+    './asset/me1.jpg',
+    './asset/me1.jpg',
+    './asset/me1.jpg',
+    './asset/me1.jpg',
+    './asset/me1.jpg',
+    './asset/me1.jpg',
+].map(pic => {
+    return new THREE.MeshLambertMaterial({ map: loader.load(pic) });
+});
+const geometry = new THREE.BoxGeometry(8, 8, 8);
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+controls.enablePan = false;
+controls.enableRotate = true;
+controls.enableZoom = false;
+
+// 애니메이션 루프
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.y += 0.01; // y축 기준 회전
+    cube.rotation.x += 0.005; // x축도 살짝 회전
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+animate();
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".last_s",
+        start: "top center",
+        end: "top 20%",
+        scroller: scrollContainer,
+        scrub: true,
+    }
+})
+    .to(".p_cube_wrap", {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        ease: "power1.out"
+    });
+
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".last_t",
+        start: "top center",
+        end: "bottom center",
+        scroller: scrollContainer,
+        scrub: true,
+    }
+})
+    .to(".p_cube_wrap", {
+        top: 150 + '%',
+        left: 20 + '%',
+        scale: 2,
+        duration: 1,
+        ease: "power1.out"
+    });
+gsap.timeline({
+    scrollTrigger: {
+        trigger: ".last_t",
+        start: "top center",
+        end: "center center",
+        scroller: scrollContainer,
+        scrub: true,
+    }
+})
+    .to(".last_p .last_t .name", {
+        opacity: 1,
+        paddingRight: 0,
+        duration: 1,
+        ease: "power1.out"
+    });
